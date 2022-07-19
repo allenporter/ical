90e66b6af743a1ee8a45e2731a4808b4ac41bfcc
Allen Porter <allen@thebends.org>
2022-07-18 20:44:51 -0700

    Add support for additional properties (#11)
    * Improve coverage for additional property types
    
    * Update golden formatting

--- ical/properties.py
+++ ical/properties.py
@@ -5,6 +5,7 @@ This file contains properties that may appear in multiple components.
 
 from __future__ import annotations
 
+import enum
 import re
 from collections.abc import Callable
 from typing import Any, Generator
@@ -42,3 +43,32 @@ class Description(str):
             index = value_match.span()[1] + 1
             value = value[index:]
         return unescape(value)
+
+
+class EventStatus(str, enum.Enum):
+    """Status or confirmation of the event."""
+
+    CONFIRMED = "CONFIRMED"
+    TENTATIVE = "TENTATIVE"
+    CANCELLED = "CANCELLED"
+
+
+COMMA_REGEX = re.compile(',(?=([^"]*"[^"]*")*[^"]*$)')
+
+
+class PropertyList(list[str]):
+    """Specifies categories or subtypes of a calendar component."""
+
+    @classmethod
+    def __get_validators__(
+        cls,
+    ) -> Generator[Callable[[str], list[str]], None, None]:
+        yield cls.parse
+
+    @classmethod
+    def parse(cls, value: Any) -> list[str]:
+        """Parse a rfc5545 description into a str."""
+        if not isinstance(value, str):
+            raise TypeError(f"Expected categories as string: {value}")
+        # This is incorrect and does not yet handle quoted strings
+        return value.split(",")
