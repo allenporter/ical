398568c36a1a5703bc36c84ae49e1ae2877c9541
Allen Porter <allen@thebends.org>
2022-07-21 20:02:17 -0700

    Add support for parsing extra unknown properties (#13)
    * Add support for parsing extra unknown properties
    
    * Update golden files

---
+++ tests/conftest.py
@@ -0,0 +1,26 @@
+"""Test fixtures."""
+
+import dataclasses
+import datetime
+import json
+from typing import Any
+
+import pytest
+
+
+class DataclassEncoder(json.JSONEncoder):
+    """Class that can dump data classes as dict for comparison to golden."""
+
+    def default(self, o: Any) -> Any:
+        if isinstance(o, datetime.datetime):
+            return str(o)
+        if dataclasses.is_dataclass(o):
+            # Omit empty
+            return {k: v for (k, v) in dataclasses.asdict(o).items() if v}
+        return super().default(o)
+
+
+@pytest.fixture
+def json_encoder() -> json.JSONEncoder:
+    """Fixture that creates a json encoder."""
+    return DataclassEncoder()
--- tests/test_contentlines.py
+++ tests/test_contentlines.py
@@ -1,8 +1,6 @@
 """Tests for timeline related calendar eents."""
 
-import dataclasses
 import json
-from typing import Any
 
 import pytest
 from pytest_golden.plugin import GoldenTestFixture
@@ -10,19 +8,11 @@ from pytest_golden.plugin import GoldenTestFixture
 from ical.contentlines import parse_content
 
 
-class DataclassEncoder(json.JSONEncoder):
-    """Class that can dump data classes as dict for comparison to golden."""
-
-    def default(self, o: Any) -> Any:
-        if not dataclasses.is_dataclass(o):
-            return o
-        # Omit empty
-        return {k: v for (k, v) in dataclasses.asdict(o).items() if v}
-
-
 @pytest.mark.golden_test("testdata/contentlines/*.yaml")
-def test_golden_files(golden: GoldenTestFixture) -> None:
+def test_golden_files(
+    golden: GoldenTestFixture, json_encoder: json.JSONEncoder
+) -> None:
     """Fixture to read golden file and compare to golden output."""
     values = parse_content(golden["input"])
-    values = json.loads(DataclassEncoder().encode(values))
+    values = json.loads(json_encoder.encode(values))
     assert values == golden["output"]
