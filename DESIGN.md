5ffc6634de52b2455d59093366b4110fbe47e7ec
Allen Porter <allen@thebends.org>
2022-08-02 07:48:11 -0700

    Add a Timeline implementation capable of rrule event generation

---
+++ DESIGN.md
@@ -0,0 +1,44 @@
+# Design
+
+## Overall Design
+
+TODO: Replace with overall design
+
+## Recurrence
+
+The design for recurrence was based on the [design guidance](https://github.com/bmoeskau/Extensible/blob/master/recurrence-overview.md) from Calendar Pro.
+
+### Application Goals
+
+The motivation is to support most simple use cases (e.g. home and small
+business applications) that require repeating events such as daily, weekly,
+monthly. The other lesser used aspects of the rfc5545 recurrence format
+like secondly, minutely, hourly or yearly are not needed for these types
+of use cases.
+
+There are performance implications based on possible event storage and
+event generation trade-offs. The initial approach will be to design for
+simplicity first, targeting smaller calendars (e.g. tens of recurring
+events, not thousands) and may layer in performance optimizations such
+as caching later, under the existing APIs.
+
+### Recurrence Format
+
+Like other components in this library, the recurrence format is parsed into
+a data object using pydantic. This library has no additional internal
+storage. An rrule is stored as a column of an event.
+
+### Event Generation
+
+The `Timeline` interface is used to iterate over events, and can also work
+to abstract away the details of recurring events. Recurring events may
+repeat indefinitely, imply the existing iterator only based may need careful
+consideration e.g. it can't serialize all events into the sorted heapq.
+
+The python library `dateutil` has an [rrule](https://dateutil.readthedocs.io/en/stable/rrule.html)
+module with a lightweight and complete implementation of recurrence rules.
+
+
+### Recurrence Editing
+
+WIP
--- ical/timeline.py
+++ ical/timeline.py
@@ -4,26 +4,31 @@ from __future__ import annotations
 
 import datetime
 import heapq
+import logging
 from collections.abc import Iterable, Iterator
 
+from dateutil import rrule
+
 from .event import Event
+from .types import Frequency, Weekday
+
+_LOGGER = logging.getLogger(__name__)
 
 
 class Timeline(Iterable[Event]):
     """A set of events on a calendar."""
 
-    def __init__(self, events: list[Event]) -> None:
+    def __init__(self, iterable: Iterable[Event]) -> None:
         """Initialize timeline."""
-        self._events = events
+        self._iterable = iterable
 
     def __iter__(self) -> Iterator[Event]:
         """Return an iterator as a traversal over events in chronological order."""
-
         # Using a heap is faster than sorting if the number of events (n) is
         # much bigger than the number of events we extract from the iterator (k).
         # Complexity: O(n + k log n).
         heap: list[tuple[datetime.date | datetime.datetime, Event]] = []
-        for event in self._events:
+        for event in iter(self._iterable):
             heapq.heappush(heap, (event.start, event))
         while heap:
             (_, event) = heapq.heappop(heap)
@@ -81,3 +86,81 @@ class Timeline(Iterable[Event]):
     def now(self) -> Iterator[Event]:
         """Return an iterator containing all events active on the specified day."""
         return self.at_instant(datetime.datetime.now())
+
+
+class RecurIterator(Iterator[Event]):
+    """An iterator for a recurrence rule."""
+
+    def __init__(
+        self, event: Event, recur: Iterator[datetime.datetime | datetime.date]
+    ):
+        """Initialize the RecurIterator."""
+        self._event = event
+        self._event_duration = event.computed_duration
+        self._recur = recur
+        self._is_all_day = not isinstance(self._event.dtstart, datetime.datetime)
+
+    def __next__(self) -> Event:
+        """Return the next event in the recurrence."""
+        dtstart: datetime.datetime | datetime.date = next(self._recur)
+        if self._is_all_day and isinstance(dtstart, datetime.datetime):
+            dtstart = dtstart.date()
+        return self._event.copy(
+            deep=True,
+            update={
+                "dtstart": dtstart,
+                "dtend": dtstart + self._event_duration,
+            },
+        )
+
+
+class RecurIterable(Iterable[Event]):
+    """A series of events from a recurring event."""
+
+    def __init__(self, event: Event, recur: rrule.rrule) -> None:
+        """Initialize timeline."""
+        self._event = event
+        self._recur = recur
+
+    def __iter__(self) -> Iterator[Event]:
+        """Return an iterator as a traversal over events in chronological order."""
+        return RecurIterator(self._event, iter(self._recur))
+
+
+RRULE_FREQ = {
+    Frequency.DAILY: rrule.DAILY,
+    Frequency.WEEKLY: rrule.WEEKLY,
+    Frequency.MONTHLY: rrule.MONTHLY,
+}
+RRULE_WEEKDAY = {
+    Weekday.MONDAY: rrule.MO,
+    Weekday.TUESDAY: rrule.TU,
+    Weekday.WEDNESDAY: rrule.WE,
+    Weekday.THURSDAY: rrule.TH,
+    Weekday.FRIDAY: rrule.FR,
+    Weekday.SATURDAY: rrule.SA,
+    Weekday.SUNDAY: rrule.SU,
+}
+
+
+def recur_timeline(event: Event) -> Timeline:
+    """Create a timeline for a recurring event."""
+    if not event.rrule:
+        raise ValueError("Event did not have rrule")
+    if not (freq := RRULE_FREQ.get(event.rrule.freq)):
+        raise ValueError(f"Unsupported frequency in rrule: {event.rrule}")
+
+    byweekday: list[rrule.weekday] | None = None
+    if event.rrule.by_week_day:
+        byweekday = [RRULE_WEEKDAY[week_day] for week_day in event.rrule.by_week_day]
+    recur = rrule.rrule(
+        freq=freq,
+        dtstart=event.dtstart,
+        interval=event.rrule.interval,
+        count=event.rrule.count,
+        until=event.rrule.until,
+        byweekday=byweekday,
+        bymonthday=event.rrule.by_month_day,
+    )
+    _LOGGER.debug("Creating timeline for rrule=%s", recur)
+    return Timeline(RecurIterable(event, recur))
