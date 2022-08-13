e0f193bd87269fe1ac881441c3f90dfd39104c01
Allen Porter <allen@thebends.org>
2022-08-12 23:14:57 -0700

    Read system timezone information with fallback to tzdata

--- ical/tzif/timezoneinfo.py
+++ ical/tzif/timezoneinfo.py
@@ -1,5 +1,14 @@
-"""Library for returning details about a timezone."""
+"""Library for returning details about a timezone.
 
+This package follows the same approach as zoneinfo for loading timezone
+data. It first checks the system TZPATH, then falls back to the tzdata
+python package.
+"""
+
+from __future__ import annotations
+
+import os
+import zoneinfo
 from functools import cache
 from importlib import resources
 
@@ -7,16 +16,36 @@ from .model import TimezoneInfo
 from .tzif import read_tzif
 
 
+class TimezoneInfoError(Exception):
+    """Raised on error working with timezone information."""
+
+
+def _find_tzfile(key: str) -> str | None:
+    """Retrieve the path to a TZif file from a key."""
+    if key not in zoneinfo.available_timezones():
+        return None
+
+    for search_path in zoneinfo.TZPATH:
+        filepath = os.path.join(search_path, key)
+        if os.path.isfile(filepath):
+            return filepath
+
+    return None
+
+
 @cache
-def read_timezones() -> set[str]:
+def read_tzdata_timezones() -> set[str]:
     """Returns the set of valid timezones."""
-    with resources.files("tzdata").joinpath("zones").open(
-        "r", encoding="utf-8"
-    ) as zones_file:
-        return {line.strip() for line in zones_file.readlines()}
+    try:
+        with resources.files("tzdata").joinpath("zones").open(
+            "r", encoding="utf-8"
+        ) as zones_file:
+            return {line.strip() for line in zones_file.readlines()}
+    except ModuleNotFoundError as err:
+        raise TimezoneInfoError("Unable to read timezone from tzdata") from err
 
 
-def iana_key_to_resource(key: str) -> tuple[str, str]:
+def _iana_key_to_resource(key: str) -> tuple[str, str]:
     """Returns the package and resource file for the specified timezone."""
     if "/" not in key:
         return "tzdata.zoneinfo", key
@@ -27,6 +56,13 @@ def iana_key_to_resource(key: str) -> tuple[str, str]:
 
 def read(key: str) -> TimezoneInfo:
     """Read the TZif file from the tzdata package and return timezone records."""
-    (package, resource) = iana_key_to_resource(key)
+    # First attempt local filesystem read for timezone
+    tzfile = _find_tzfile(key)
+    if tzfile is not None:
+        with open(tzfile, "rb") as tzfile_file:
+            return read_tzif(tzfile_file.read())
+
+    # Fallback to tzdata package
+    (package, resource) = _iana_key_to_resource(key)
     with resources.files(package).joinpath(resource).open("rb") as tzdata_file:
         return read_tzif(tzdata_file.read())
--- tests/tzif/test_zoneinfo.py
+++ tests/tzif/test_zoneinfo.py
@@ -7,13 +7,6 @@ import pytest
 from ical.tzif import timezoneinfo
 
 
-@pytest.mark.parametrize("key", timezoneinfo.read_timezones())
-def test_all_tzdata(key: str) -> None:
-    """Verify that all available timezones in the system have valid tzdata."""
-    result = timezoneinfo.read(key)
-    assert result.rule or result.transitions or result.leap_seconds
-
-
 @pytest.mark.parametrize("key", zoneinfo.available_timezones())
 def test_all_zoneinfo(key: str) -> None:
     """Verify that all available timezones in the system have valid tzdata."""
