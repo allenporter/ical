16d613c52b7b033a24bd421049b6ebc95752da80
Allen Porter <allen@thebends.org>
2022-08-28 09:01:55 -0700

    Typing and comment cleanup

--- ical/types/__init__.py
+++ ical/types/__init__.py
@@ -1,9 +1,10 @@
-"""Library for parsing rfc5545 types."""
+"""Library for parsing rfc5545 Property Value Data Types."""
 
 # Import all types for the registry
+from . import integer  # noqa: F401
 from . import boolean, date, date_time, duration  # noqa: F401
 from . import float as float_pkg  # noqa: F401
-from . import geo, integer  # noqa: F401
+from .geo import Geo
 from .period import Period
 
-__all__ = ["Period"]
+__all__ = ["Period", "Geo"]
