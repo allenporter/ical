ef6b054402ced83212f7f1530cfe9897f16140f3
Allen Porter <allen.porter@gmail.com>
2022-10-21 20:44:23 -0700

    Remove timezone context manager with improved timespan/timeline APIs (#124)
    * Update timespan/timeline APIs to better accept attendee timezone
    
    * Fix timestamp in calendar
    
    * Update timespan docs

--- ical/__init__.py
+++ ical/__init__.py
@@ -11,6 +11,7 @@ __all__ = [
     "journal",
     "store",
     "timeline",
+    "timespan",
     "timezone",
     "todo",
     "types",
--- ical/util.py
+++ ical/util.py
@@ -4,19 +4,14 @@ from __future__ import annotations
 
 import datetime
 import uuid
-from collections.abc import Generator
-from contextlib import contextmanager
-from contextvars import ContextVar
 
 __all__ = [
-    "use_local_timezone",
     "dtstamp_factory",
     "uid_factory",
 ]
 
 
 MIDNIGHT = datetime.time()
-LOCAL_TZ = ContextVar[datetime.tzinfo]("_local_tz")
 
 
 def dtstamp_factory() -> datetime.datetime:
@@ -29,61 +24,19 @@ def uid_factory() -> str:
     return str(uuid.uuid1())
 
 
-@contextmanager
-def use_local_timezone(local_tz: datetime.tzinfo) -> Generator[None, None, None]:
-    """Set the local timezone to use when converting a date to datetime.
-
-    This is expected to be used as a context manager when the default timezone
-    used by python is not the timezone to be used for calendar operations (the
-    attendees local timezone).
-
-    Example:
-    ```
-    import datetime
-    import zoneinfo
-    from ical.calendar import Calendar
-    from ical.event import Event
-    from ical.util import use_local_timezone
-
-    cal = Calendar()
-    cal.events.append(
-        Event(
-            summary="Example",
-            start=datetime.date(2022, 2, 1),
-            end=datetime.date(2022, 2, 2)
-        )
-    )
-    # Use UTC-8 as local timezone
-    with use_local_timezone(zoneinfo.ZoneInfo("America/Los_Angeles")):
-        # Returns event above
-        events = cal.timeline.start_after(
-            datetime.datetime(2022, 2, 1, 7, 59, 59, tzinfo=datetime.timezone.utc))
-
-        # Does not return event above
-        events = cal.timeline.start_after(
-            datetime.datetime(2022, 2, 1, 8, 00, 00, tzinfo=datetime.timezone.utc))
-    ```
-    """
-    orig_tz = LOCAL_TZ.set(local_tz)
-    try:
-        yield
-    finally:
-        LOCAL_TZ.reset(orig_tz)
-
-
 def local_timezone() -> datetime.tzinfo:
     """Get the local timezone to use when converting date to datetime."""
-    if local_tz := LOCAL_TZ.get(None):
-        return local_tz
     if local_tz := datetime.datetime.now().astimezone().tzinfo:
         return local_tz
     return datetime.timezone.utc
 
 
-def normalize_datetime(value: datetime.date | datetime.datetime) -> datetime.datetime:
+def normalize_datetime(
+    value: datetime.date | datetime.datetime, tzinfo: datetime.tzinfo | None = None
+) -> datetime.datetime:
     """Convert date or datetime to a value that can be used for comparison."""
     if not isinstance(value, datetime.datetime):
         value = datetime.datetime.combine(value, MIDNIGHT)
     if value.tzinfo is None:
-        value = value.replace(tzinfo=local_timezone())
+        value = value.replace(tzinfo=tzinfo if tzinfo else local_timezone())
     return value
