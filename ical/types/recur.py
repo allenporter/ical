419f240cc7c4b5bcf432f7ce98cc796240af157e
Allen Porter <allen.porter@gmail.com>
2023-10-21 10:18:06 -0700

    Use action for ruff and separate lint workflow (#234)
    * Use action for ruff and separate lint workflow
    
    * Fix weekday type
    
    * Fix typos

--- ical/types/recur.py
+++ ical/types/recur.py
@@ -92,7 +92,7 @@ class WeekdayValue:
         return f"{self.occurrence or ''}{self.weekday}"
 
 
-    def as_rrule_weekday(self) -> weekday:
+    def as_rrule_weekday(self) -> rrule.weekday:
         """Convert the occurrence to a weekday value."""
         wd = RRULE_WEEKDAY[self.weekday]
         if self.occurrence is None:
@@ -293,7 +293,7 @@ class Recur(BaseModel):
 
     @classmethod
     def __encode_property_value__(cls, data: dict[str, Any]) -> str:
-        """Encode the recurence rule in ICS format."""
+        """Encode the recurrence rule in ICS format."""
         result = []
         for key, value in data.items():
             # Need to encode based on field type also using json encoders
