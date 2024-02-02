77bd53053bf3608b35f9239c3111d15b25b00ee4
Allen Porter <allen.porter@gmail.com>
2024-02-02 07:34:32 +0000

    Add view for repeated todo items

---
+++ ical/list.py
@@ -0,0 +1,131 @@
+"""A List is a set of objects on a calendar.
+
+A List is used to iterate over all objects, including expanded recurring
+objects. A List is similar to a Timeline, except it does not repeat recurring
+objects on the list and they are only shown once. A list does not repeat
+forever.
+"""
+
+import datetime
+from collections.abc import Generator, Iterable
+import logging
+
+from .todo import Todo
+from .iter import (
+    LazySortableItem,
+    MergedIterable,
+    RecurIterable,
+    SortableItem,
+    SortableItemValue,
+    SortedItemIterable,
+)
+from .types.recur import RecurrenceId
+
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class RecurAdapter:
+    """An adapter that expands an Todo instance for a recurrence rule.
+
+    This adapter is given an todo, then invoked with a specific date/time instance
+    that the todo is due from a recurrence rule. The todo is copied with
+    necessary updated fields to act as a flattened instance of the todo item.
+    """
+
+    def __init__(self, todo: Todo, tzinfo: datetime.tzinfo | None = None):
+        """Initialize the RecurAdapter."""
+        self._todo = todo
+        self._duration = todo.computed_duration
+        self._tzinfo = tzinfo
+
+    def get(
+        self, dtstart: datetime.datetime | datetime.date
+    ) -> SortableItem[datetime.datetime, Todo]:
+        """Return a lazy sortable item."""
+
+        recur_id_dt = dtstart
+        # Make recurrence_id floating time to avoid dealing with serializing
+        # TZID. This value will still be unique within the series and is in
+        # the context of dtstart which may have a timezone.
+        if isinstance(recur_id_dt, datetime.datetime) and recur_id_dt.tzinfo:
+            recur_id_dt = recur_id_dt.replace(tzinfo=None)
+        recurrence_id = RecurrenceId.__parse_property_value__(recur_id_dt)
+
+        def build() -> Todo:
+            return self._todo.copy(
+                update={
+                    "dtstart": dtstart,
+                    "due": dtstart + self._duration,
+                    "recurrence_id": recurrence_id,
+                },
+            )
+
+        return LazySortableItem(dtstart, build)
+
+
+def _todos_by_uid(todos: list[Todo]) -> dict[str, list[Todo]]:
+    todos_by_uid: dict[str, list[Todo]] = {}
+    for todo in todos:
+        if todo.uid is None:
+            raise ValueError("Todo must have a UID")
+        if todo.uid not in todos_by_uid:
+            todos_by_uid[todo.uid] = []
+        todos_by_uid[todo.uid].append(todo)
+    return todos_by_uid
+
+
+def _todo_iterable(
+    iterable: list[Todo], tzinfo: datetime.tzinfo
+) -> Iterable[SortableItem[datetime.datetime, Todo]]:
+    """Create a sorted iterable from the list of events."""
+
+    def sortable_items() -> (
+        Generator[SortableItem[datetime.datetime, Todo], None, None]
+    ):
+        for todo in iterable:
+            if todo.recurring:
+                continue
+            yield SortableItemValue(todo.due, todo)
+
+    return SortedItemIterable(sortable_items, tzinfo)
+
+
+def _pick_todo(todos: list[Todo], tzinfo: datetime.tzinfo) -> Todo:
+    """Pick a todo to return in a list from a list of recurring todos.
+
+    The items passed in must all be for the same original todo (either a
+    single todo or instance of a recurring todo including any edits). An
+    edited instance of a recurring todo has a recurrence-id that is
+    different from the original todo. This function will return the
+    next todo that is incomplete and has the latest due date.
+
+
+    """
+    # For a recurring todo, the dtstart is after the last due date. Therefore
+    # we can stort items by dtstart and pick the last one that hasn't happened
+    iters = [_todo_iterable(todos, tzinfo=tzinfo)]
+    for todo in todos:
+        if not (recur := todo.as_rrule()):
+            continue
+        iters.append(RecurIterable(RecurAdapter(todo, tzinfo=tzinfo).get, recur))
+
+    root_iter = MergedIterable(iters)
+    # Pick the first todo that hasn't started yet based on its dtstart
+    now = datetime.datetime.now(tzinfo)
+    last: Todo | None = None
+    todo = next(iter(root_iter))
+    for todo in root_iter:
+        if todo.item.start_datetime > now:
+            break
+        last = todo.item
+    return last
+
+
+def todo_list_view(
+    todos: list[Todo], tzinfo: datetime.tzinfo
+) -> Generator[Todo, None, None]:
+    """Create a list view for todos on a calendar, including recurrence."""
+    todos_by_uid = _todos_by_uid(todos)
+    for todos in todos_by_uid.values():
+        yield _pick_todo(todos, tzinfo=tzinfo)
---
+++ tests/test_list.py
@@ -0,0 +1,107 @@
+"""Tests for list view of todo items."""
+
+import datetime
+
+import freezegun
+import pytest
+
+from ical.list import todo_list_view
+from ical.todo import Todo
+from ical.types.recur import Recur
+
+
+def test_empty_list() -> None:
+    """Test an empty list."""
+    view = todo_list_view([], tzinfo=datetime.timezone.utc)
+    assert list(view) == []
+
+
+@pytest.mark.parametrize(
+    ("status"),
+    [
+        ("NEEDS-ACTION"),
+        ("IN-PROCESS"),
+    ],
+)
+def test_daily_recurring_item_due_today_incomplete(status: str) -> None:
+    """Test a daily recurring item that is due today ."""
+    with freezegun.freeze_time("2024-01-10T10:05:00-05:00"):
+        todo = Todo(
+            dtstart=datetime.date.today() - datetime.timedelta(days=1),
+            summary="Daily incomplete",
+            due=datetime.date.today(),
+            rrule=Recur.from_rrule("FREQ=DAILY"),
+            status=status,
+        )
+        view = list(todo_list_view([todo], tzinfo=datetime.timezone.utc))
+
+    assert len(view) == 1
+    assert view[0].summary == todo.summary
+    assert view[0].dtstart == datetime.date(2024, 1, 10)
+    assert view[0].due == datetime.date(2024, 1, 11)
+    assert view[0].recurrence_id == "20240110"
+
+
+@pytest.mark.parametrize(
+    ("status"),
+    [
+        ("NEEDS-ACTION"),
+        ("IN-PROCESS"),
+    ],
+)
+def test_daily_recurring_item_due_tomorrow(status: str) -> None:
+    """Test a daily recurring item that is due tomorrow."""
+    with freezegun.freeze_time("2024-01-10T10:05:00-05:00"):
+        todo = Todo(
+            dtstart=datetime.date.today(),
+            summary="Daily incomplete",
+            due=datetime.date.today() + datetime.timedelta(days=1),
+            rrule=Recur.from_rrule("FREQ=DAILY"),
+            status=status,
+        )
+        view = list(todo_list_view([todo], tzinfo=datetime.timezone.utc))
+
+    assert len(view) == 1
+    assert view[0].summary == todo.summary
+    assert view[0].dtstart == datetime.date(2024, 1, 10)
+    assert view[0].due == datetime.date(2024, 1, 11)
+    assert view[0].recurrence_id == "20240110"
+
+
+@pytest.mark.parametrize(
+    ("status"),
+    [
+        ("NEEDS-ACTION"),
+        ("IN-PROCESS"),
+    ],
+)
+def test_daily_recurring_item_due_yesterday(status: str) -> None:
+    """Test a daily recurring item that is due yesterday ."""
+
+    with freezegun.freeze_time("2024-01-10T10:05:00-05:00"):
+        todo = Todo(
+            dtstart=datetime.date.today() - datetime.timedelta(days=1),
+            summary="Daily incomplete",
+            due=datetime.date.today(),
+            rrule=Recur.from_rrule("FREQ=DAILY"),
+            status=status,
+        )
+        view = list(todo_list_view([todo], tzinfo=datetime.timezone.utc))
+
+    # The item should be returned with a recurrence_id of today
+    assert len(view) == 1
+    assert view[0].summary == todo.summary
+    assert view[0].dtstart == datetime.date(2024, 1, 10)
+    assert view[0].due == datetime.date(2024, 1, 11)
+    assert view[0].recurrence_id == "20240110"
+    assert view[0].status == status
+
+    with freezegun.freeze_time("2024-01-11T08:05:00-05:00"):
+        view = list(todo_list_view([todo], tzinfo=datetime.timezone.utc))
+
+    assert len(view) == 1
+    assert view[0].summary == todo.summary
+    assert view[0].dtstart == datetime.date(2024, 1, 11)
+    assert view[0].due == datetime.date(2024, 1, 12)
+    assert view[0].recurrence_id == "20240111"
+    assert view[0].status == status
