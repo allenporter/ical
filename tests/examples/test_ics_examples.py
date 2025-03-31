aa0994e1e725f1af9b683e9dbac7eeaf1f9d3651
Allen Porter <allen.porter@gmail.com>
2025-03-30 19:23:15 -0700

    Update tests to sort files since glob is not deterministic

--- tests/examples/test_ics_examples.py
+++ tests/examples/test_ics_examples.py
@@ -10,7 +10,7 @@ from ical.calendar_stream import IcsCalendarStream
 TEST_DIR = Path("tests/examples")
 
 
-@pytest.mark.parametrize("filename", list(TEST_DIR.glob("testdata/*.ics")))
+@pytest.mark.parametrize("filename", sorted(list(TEST_DIR.glob("testdata/*.ics"))))
 def test_parse(filename: Path, snapshot: SnapshotAssertion) -> None:
     """Test to read golden files and verify they are parsed."""
     with filename.open() as ics_file:
