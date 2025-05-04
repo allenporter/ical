6b282eeaccc2a5b4a32f5c689622677f09527d42
Allen Porter <allen.porter@gmail.com>
2025-05-04 09:01:55 -0700

    Add an example that reproduces a recurring event issue (#470)

--- tests/examples/test_ics_examples.py
+++ tests/examples/test_ics_examples.py
@@ -1,6 +1,7 @@
 """Test that parses .ics files from known good repositories."""
 
 from pathlib import Path
+from itertools import islice
 
 import pytest
 from syrupy import SnapshotAssertion
@@ -8,12 +9,23 @@ from syrupy import SnapshotAssertion
 from ical.calendar_stream import IcsCalendarStream
 
 TEST_DIR = Path("tests/examples")
+TEST_FILES = sorted(list(TEST_DIR.glob("testdata/*.ics")))
+TEST_IDS = [x.stem for x in TEST_FILES]
 
 
-@pytest.mark.parametrize("filename", sorted(list(TEST_DIR.glob("testdata/*.ics"))))
+@pytest.mark.parametrize("filename", TEST_FILES, ids=TEST_IDS)
 def test_parse(filename: Path, snapshot: SnapshotAssertion) -> None:
     """Test to read golden files and verify they are parsed."""
     with filename.open() as ics_file:
         calendar = IcsCalendarStream.calendar_from_ics(ics_file.read())
 
     assert IcsCalendarStream.calendar_to_ics(calendar) == snapshot
+
+
+@pytest.mark.parametrize("filename", TEST_FILES, ids=TEST_IDS)
+def test_iterate_events(filename: Path, snapshot: SnapshotAssertion) -> None:
+    """Test to read golden files and verify they are parsed."""
+    with filename.open() as ics_file:
+        calendar = IcsCalendarStream.calendar_from_ics(ics_file.read())
+    
+    assert list(islice(iter(calendar.timeline), 5)) == snapshot
