6622a4095e27669174ba4f99a7324965626c67c9
Rafał Krupiński <zloto@fastmail.com>
2025-10-23 17:15:19 +0200

    Don't require due in recurrent Todos

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -74,28 +74,7 @@ def test_duration() -> None:
         ({}),
         (
             {
-                "start": datetime.datetime(2022, 9, 6, 6, 0, 0),
-            }
-        ),
-        (
-            {
                 "due": datetime.datetime(2022, 9, 6, 6, 0, 0),
-            }
-        ),
-        (
-            {
-                "start": datetime.datetime(2022, 9, 6, 6, 0, 0),
-                "due": datetime.datetime(
-                    2022, 9, 7, 6, 0, 0, tzinfo=zoneinfo.ZoneInfo("America/Regina")
-                ),
-            }
-        ),
-        (
-            {
-                "start": datetime.datetime(
-                    2022, 9, 6, 6, 0, 0, tzinfo=zoneinfo.ZoneInfo("America/Regina")
-                ),
-                "due": datetime.datetime(2022, 9, 7, 6, 0, 0),  # floating
             }
         ),
         (
