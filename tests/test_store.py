8e73d6b27a1a9cfa0894cf9c94a6233548fdb38f
Allen Porter <allen.porter@gmail.com>
2026-02-14 15:19:49 -0800

    Adjusts spelling ignore rules

--- tests/test_store.py
+++ tests/test_store.py
@@ -1935,4 +1935,6 @@ def test_control_characters(
     new_calendar = IcsCalendarStream.calendar_from_ics(ics)
     assert len(new_calendar.events) == 1
     persisted_event = new_calendar.events[0]
-    assert persisted_event.summary == "Hello, Youre seeing an invalid character"  # codespell:ignore Youre
+    assert (
+        persisted_event.summary == "Hello, Youre seeing an invalid character"
+    )  # codespell:ignore
